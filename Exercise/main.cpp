f0fd560b80bc035f23f2708dfe66aef0ba777463
Kartikeya Dubey <kartikeyadubey2@gmail.com>
2023-10-22 02:56:11 +0530

    Fixed Controls, added function to make a cuboid

--- Exercise/main.cpp
+++ Exercise/main.cpp
@@ -11,12 +11,16 @@
 static int slices = 16;
 static int stacks = 16;
 
-glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);      // Initial pos
-glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);   // Initial target
+glm::vec3 initialCameraPos(0.0f, 0.0f, 3.0f);      // Initial pos
+glm::vec3 initialCameraTarget(0.0f, 0.0f, 0.0f);   // Initial target
+glm::vec3 initialUpVector(0.0f, 1.0f, 0.0f);       // Initial Up vector
+
+glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);      // pos
+glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);   // target
 glm::vec3 upVector(0.0f, 1.0f, 0.0f);       // Up vector
 glm::mat4 viewMatrix;
 
-float moveSpeed = 0.09f;
+float moveSpeed = 0.5f;
 float rotateSpeed = 0.09f;
 
 float yawAngle = 0.0f;      // n axis
@@ -24,17 +28,37 @@ float pitchAngle = 0.0f;    // u axis
 float rollAngle = 0.0f;     // v axis
 
 /* GLUT callback Handlers */
+void initCam()
+{
+    yawAngle = 0.0f;
+    pitchAngle = 0.0f;
+    rollAngle = 0.0f;
+
+    cameraPos = initialCameraPos;
+    cameraTarget = initialCameraTarget;
+    upVector = initialUpVector;
+
+    viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadMatrixf(glm::value_ptr(viewMatrix));
+}
+
+
 void updateView()
 {
     glm::vec3 n = glm::normalize(cameraPos - cameraTarget);
     glm::vec3 u = glm::normalize(glm::cross(upVector, n));
     glm::vec3 v = glm::cross(n, u);
 
-    glm::mat4 yawRot = glm::rotate(glm::mat4(1.0f), yawAngle, upVector);
-    glm::mat4 pitchRot = glm::rotate(glm::mat4(1.0f), pitchAngle, u);
-    glm::mat4 rollRot = glm::rotate(glm::mat4(1.0f), rollAngle, n);
+    glm::quat yawQuat = glm::angleAxis(yawAngle, upVector);
+    glm::quat pitchQuat = glm::angleAxis(pitchAngle, u);
+    glm::quat rollQuat = glm::angleAxis(rollAngle, n);
 
-    glm::mat4 rotationMat = yawRot * pitchRot * rollRot;
+    glm::quat rotationQuat = yawQuat * pitchQuat * rollQuat;
+    rotationQuat = glm::normalize(rotationQuat);
+
+    glm::mat4 rotationMat = glm::mat4_cast(rotationQuat);
 
     cameraTarget = cameraPos - glm::vec3(rotationMat * glm::vec4(n, 0.0f));
     upVector = glm::vec3(rotationMat * glm::vec4(upVector, 0.0f));
@@ -51,6 +75,54 @@ void updateView()
     //std::cout << "n - " << glm::to_string(n) << " u - " << glm::to_string(u) << " v - " << glm::to_string(v) << "\n";
 }
 
+// Takes bottom-left-front vertex and top-right-back vertex and draws a cuboid
+void drawCuboid(float x1, float y1, float z1, float x2, float y2, float z2, int normal)
+{   // normal = 1 is outward normals, normal = -1 is inwards
+    glBegin(GL_QUADS);
+
+        //Front Face
+        glNormal3f(0.0, 0.0, normal*1.0);
+        glVertex3f(x1, y1, z1);
+        glVertex3f(x2, y1, z1);
+        glVertex3f(x2, y2, z1);
+        glVertex3f(x1, y2, z1);
+
+        // Right Face
+        glNormal3f(normal*1.0, 0.0, 0.0);
+        glVertex3f(x2, y1, z1);
+        glVertex3f(x2, y1, z2);
+        glVertex3f(x2, y2, z2);
+        glVertex3f(x2, y2, z1);
+
+        // Back Face
+        glNormal3f(0.0, 0.0, normal*-1.0);
+        glVertex3f(x1, y1, z2);
+        glVertex3f(x2, y1, z2);
+        glVertex3f(x2, y2, z2);
+        glVertex3f(x1, y2, z2);
+
+        // Left Face
+        glNormal3f(normal*-1.0, 0.0, 0.0);
+        glVertex3f(x1, y1, z1);
+        glVertex3f(x1, y1, z2);
+        glVertex3f(x1, y2, z2);
+        glVertex3f(x1, y2, z1);
+
+        // Bottom Face
+        glNormal3f(0.0, normal*-1.0, 0.0);
+        glVertex3f(x1, y1, z1);
+        glVertex3f(x2, y1, z1);
+        glVertex3f(x2, y1, z2);
+        glVertex3f(x1, y1, z2);
+
+        // Top Face
+        glNormal3f(0.0, normal*1.0, 0.0);
+        glVertex3f(x1, y2, z1);
+        glVertex3f(x2, y2, z1);
+        glVertex3f(x2, y2, z2);
+        glVertex3f(x1, y2, z2);
+    glEnd();
+}
 
 static void resize(int width, int height)
 {
@@ -75,56 +147,22 @@ static void resize(int width, int height)
     glLoadIdentity() ;
 }
 
+void setColorRGB(int R, int G, int B)
+{
+    glColor3f(R/255.0, G/255.0, B/255.0);
+}
+
 static void display(void)
 {
     const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
     const double a = t*90.0;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
 
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
+    setColorRGB(244, 247, 208);
+    drawCuboid(-10.0, 0.0, 3.0, 10.0, 10.0, -10.0, -1);
 
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
+    glFlush();
     glutSwapBuffers();
 }
 
@@ -134,6 +172,10 @@ static void key(unsigned char key, int x, int y)
     glm::vec3 moveBy;
     glm::mat4 viewMatrix;
 
+    glm::vec3 n = glm::normalize(cameraPos - cameraTarget);
+    glm::vec3 u = glm::normalize(glm::cross(upVector, n));
+    glm::vec3 v = glm::cross(n, u);
+
     switch (key)
     {
         case 27 :
@@ -141,30 +183,9 @@ static void key(unsigned char key, int x, int y)
             exit(0);
             break;
 
+        // Forwards or backwards
         case 'w':
-            moveBy = moveSpeed * (cameraTarget - cameraPos);
-            cameraPos += moveBy;
-            cameraTarget += moveBy;
-            upVector += moveBy;
-
-            viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
-            glMatrixMode(GL_MODELVIEW);
-            glLoadMatrixf(glm::value_ptr(viewMatrix));
-            //updateView();
-            break;
-        case 's':
-            moveBy = moveSpeed * (cameraTarget - cameraPos);
-            cameraPos -= moveBy;
-            cameraTarget -= moveBy;
-            upVector -= moveBy;
-
-            viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
-            glMatrixMode(GL_MODELVIEW);
-            glLoadMatrixf(glm::value_ptr(viewMatrix));
-            //updateView();
-            break;
-        case 'd':
-            moveBy = moveSpeed * glm::normalize(glm::cross(upVector, cameraTarget - cameraPos));
+            moveBy = moveSpeed * n;//(cameraTarget - cameraPos);
             cameraPos -= moveBy;
             cameraTarget -= moveBy;
             //upVector -= moveBy;
@@ -174,11 +195,59 @@ static void key(unsigned char key, int x, int y)
             glLoadMatrixf(glm::value_ptr(viewMatrix));
             //updateView();
             break;
-        case 'a':
-            moveBy = moveSpeed * glm::normalize(glm::cross(upVector, cameraTarget - cameraPos));
+        case 's':
+            moveBy = moveSpeed * n;// (cameraTarget - cameraPos);
             cameraPos += moveBy;
             cameraTarget += moveBy;
             //upVector += moveBy;
+
+            viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadMatrixf(glm::value_ptr(viewMatrix));
+            //updateView();
+            break;
+
+        // Left and right
+        case 'a':
+            moveBy = moveSpeed * u;//glm::normalize(glm::cross(upVector, cameraTarget - cameraPos));
+            cameraPos -= moveBy;
+            cameraTarget -= moveBy;
+            //upVector -= moveBy;
+
+            viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadMatrixf(glm::value_ptr(viewMatrix));
+            //updateView();
+            break;
+        case 'd':
+            moveBy = moveSpeed * u;//glm::normalize(glm::cross(upVector, cameraTarget - cameraPos));
+            cameraPos += moveBy;
+            cameraTarget += moveBy;
+            //upVector += moveBy;
+
+            viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadMatrixf(glm::value_ptr(viewMatrix));
+            //updateView();
+            break;
+
+        // Up and Down
+        case 't':
+            moveBy = moveSpeed * v;//glm::normalize(glm::cross(upVector, cameraTarget - cameraPos));
+            cameraPos += moveBy;
+            cameraTarget += moveBy;
+            //upVector += moveBy;
+
+            viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadMatrixf(glm::value_ptr(viewMatrix));
+            //updateView();
+            break;
+        case 'g':
+            moveBy = moveSpeed * v;//glm::normalize(glm::cross(upVector, cameraTarget - cameraPos));
+            cameraPos -= moveBy;
+            cameraTarget -= moveBy;
+            //upVector -= moveBy;
 
             viewMatrix = glm::lookAt(cameraPos, cameraTarget, upVector);
             glMatrixMode(GL_MODELVIEW);
@@ -203,6 +272,7 @@ static void key(unsigned char key, int x, int y)
             pitchAngle = -rotateSpeed;
             updateView();
             break;
+
         case '3':
             rollAngle = rotateSpeed;
             updateView();
@@ -211,11 +281,16 @@ static void key(unsigned char key, int x, int y)
             rollAngle = -rotateSpeed;
             updateView();
             break;
+
+        case 'r':
+            initCam();
+            break;
+
     }
 
-    //std::cout << "Camera pos = " << glm::to_string(cameraPos) << " camera tar = " << glm::to_string(cameraTarget) << std::endl;
     glutPostRedisplay();
 }
+
 
 static void idle(void)
 {
@@ -248,13 +323,12 @@ int main(int argc, char *argv[])
     glutKeyboardFunc(key);
     glutIdleFunc(idle);
 
-
-
     glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+    //glEnable(GL_CULL_FACE);
+    //glCullFace(GL_BACK);
 
     glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
     glDepthFunc(GL_LESS);
 
     glEnable(GL_LIGHT0);
@@ -272,7 +346,6 @@ int main(int argc, char *argv[])
     glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
 
-    updateView();
     glutMainLoop();
 
     return EXIT_SUCCESS;
